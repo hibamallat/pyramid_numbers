90d7f9df988abaa89b1e4f92b31272d8d6409497
hibamallat <hibamallat040@gmail.com>
2024-10-31 13:54:57 +0200

    The file has been updated.

--- index.py
+++ index.py
@@ -1,5 +1,8 @@
 x = int(input("Enter x: "))
 
+if x <= 0:
+    print("The number must be positive")
+
 for i in range (1, x + 1): #each row from 1 up to and including x
 
     for j in range (1, i + 1) : #j goes from 1 to the current row index i

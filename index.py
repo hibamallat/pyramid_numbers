085a29ca7f6fec05a6e6cdb5489bbc6ebc684d4f
hibamallat <hibamallat040@gmail.com>
2024-11-02 02:58:23 +0200

    input validation

--- index.py
+++ index.py
@@ -1,23 +1,15 @@
-x = int(input("Enter x: "))
+try:
+    x = int(input("Enter a number: "))
 
-if x <= 0:
-    print("The number must be positive")
+    if x <= 0:
+        print("INVALID:The number should be positive")
 
-for i in range (1, x + 1): #each row from 1 up to and including x
+    for i in range (1, x + 1): #each row from 1 up to and including x
 
-    for j in range (1, i + 1) : #j goes from 1 to the current row index i
-        print(j, end = ' ')
+        for j in range (1, i + 1) : #j goes from 1 to the current row index i
+            print(j, end = ' ')
 
-    print()
+        print()
 
-
-
-
-
-
-
-
-   
-  
-
-
+except ValueError:
+    print("INVALID:Enter only a number")
